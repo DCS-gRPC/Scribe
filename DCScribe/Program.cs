56c0e7bfd0f8e5406d88ca50b42c25057ed6498e
Jeffrey Jones <jeff@jones.be>
2021-09-05 21:02:34 +0900

    Initial Serilog setup
    Setup Serilog. As part of this switch the appsettings.json to a
    configuration.yaml file since that is what OverlordBot uses and users
    will be used to it.
    
    We enable a configuration.Development.yaml but do not commit it to the
    repo since that will contain local dev settings.

--- DCScribe/Program.cs
+++ DCScribe/Program.cs
@@ -1,5 +1,8 @@
+using System;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace RurouniJones.DCScribe
 {
@@ -7,14 +10,41 @@ namespace RurouniJones.DCScribe
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            var configuration = new ConfigurationBuilder()
+                .AddYamlFile("configuration.yaml", false, true)
+                .AddYamlFile("configuration.Development.yaml", true, true)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+            try
+            {
+                Log.Information("Starting DCScribe");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Could not start DCScribe");
+            }
+            finally
+            {
+                Log.Information("Stopping DCScribe");
+                Log.CloseAndFlush();
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
-                });
+                })
+                .UseSerilog()
+                .UseWindowsService();
+        }
     }
 }
